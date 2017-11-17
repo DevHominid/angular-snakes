0acb907a83b4116c1b0dc4112c603de4bd353727
Dylan Ross Ludwick <dylan@devhominid.com>
2017-11-17 00:18:10 -0700

    define types

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  board: any;
+  boardSize: number;
+  directions: Directions;
+  snake: Snake;
+  bait: Bait;
+  score: number;
+  isGameOver: boolean;
+  initialInterval: number;
+  currentInterval: number;
+  currentDirection: number;
+  colors: Colors;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Directions {
+  left: number,
+  right: number,
+  up: number,
+  down: number
+}
+
+interface Snake {
+  direction: number,
+  sections: Sections[]
+}
+
+interface Sections {
+  x: number,
+  y: number
+}
+
+interface Bait {
+  x: number,
+  y: number
+}
+
+interface Colors {
+  gameOver: string,
+  bait: string,
+  snakeHead: string,
+  snakeBody: string,
+  board: string
 }
